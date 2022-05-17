41d411a493ee11397a1b0a1f20d87d608c0b2efb
kimpearl3599 <kimpearl3599@gmail.com>
2022-05-17 17:10:03 +0900

    201-220 solutions

--- 20220517_solutions.py
+++ 20220517_solutions.py
@@ -270,3 +270,186 @@ for price in ohlc[1:]:
     print(total)
 print(total)
 
+# 201 "비트코인" 문자열을 화면에 출력하는 print_coin() 함수를 정의하라.
+
+def print_coin():
+    print('비트코인')
+
+# 202 201번에서 정의한 함수를 호출하라.
+print_coin()
+
+# 203 201번에서 정의한 print_coin 함수를 100번 호출하라.
+for i in range(0,100):
+    print_coin()
+
+# 204 "비트코인" 문자열을 100번 화면에 출력하는 print_coins() 함수를 정의하라.
+def print_coins():
+    for i in range(0, 100):
+        print('비트코인')
+
+# 205 아래의 에러가 발생하는 이유에 대해 설명하라.
+#
+# hello()
+# def hello():
+#     print("Hi")
+# 실행 예
+#
+# NameError: name 'hello' is not defined
+# 풀이 : 함수가 정의되기 전에 함수가 호출되었기 때문에
+
+# 206 아래 코드의 실행 결과를 예측하라.
+#
+# def message() :
+#     print("A")
+#     print("B")
+#
+# message()
+# print("C")
+# message()
+# 풀이 :
+# A
+# B
+# C
+# A
+# B
+
+# 207 아래 코드의 실행 결과를 예측하라. (읽기 어려운 코드의 예입니다.)
+#
+# print("A")
+#
+# def message() :
+#     print("B")
+#
+# print("C")
+# message()
+# 풀이 :
+# A
+# C
+# B
+
+# 208 아래 코드의 실행 결과를 예측하라. (읽기 어려운 코드의 예입니다.)
+#
+# print("A")
+# def message1() :
+#     print("B")
+# print("C")
+# def message2() :
+#     print("D")
+# message1()
+# print("E")
+# message2()
+# 풀이 :
+# A
+# C
+# B
+# E
+# D
+
+# 209 아래 코드의 실행 결과를 예측하라.
+#
+# def message1():
+#     print("A")
+#
+# def message2():
+#     print("B")
+#     message1()
+#
+# message2()
+# 풀이 :
+# B
+# A
+
+# 210 아래 코드의 실행 결과를 예측하라.
+#
+# def message1():
+#     print("A")
+#
+# def message2():
+#     print("B")
+#
+# def message3():
+#     for i in range (3) :
+#         message2()
+#         print("C")
+#     message1()
+#
+# message3()
+# 풀이 :
+# B
+# C
+# B
+# C
+# B
+# C
+# A
+
+# 211 함수의 호출 결과를 예측하라.
+#
+# def 함수(문자열) :
+#     print(문자열)
+#
+# 함수("안녕")
+# 함수("Hi")
+# 풀이 :
+# 안녕
+# Hi
+
+# 212 함수의 호출 결과를 예측하라.
+#
+# def 함수(a, b) :
+#     print(a + b)
+#
+# 함수(3, 4)
+# 함수(7, 8)
+# 풀이 :
+# 7
+# 15
+
+# 213 아래와 같은 에러가 발생하는 원인을 설명하라.
+#
+# def 함수(문자열) :
+#     print(문자열)
+# 함수()
+# TypeError: 함수() missing 1 required positional argument: '문자열'
+# 풀이 : 문자열을 인자로 받는 함수를 호출할때 인자(파라미터)를 입력하지 않았기 때문에
+
+# 214 아래와 같은 에러가 발생하는 원인을 설명하라.
+#
+# def 함수(a, b) :
+#     print(a + b)
+#
+# 함수("안녕", 3)
+# TypeError: must be str, not int
+# 풀이 : 함수의 파라미터간의 타입이 달라서 연산이 되지 않기 때문에
+
+# 215 하나의 문자를 입력받아 문자열 끝에 ":D" 스마일 문자열을 이어 붙여 출력하는 print_with_smile 함수를 정의하라.
+def print_with_smile(str):
+    print(str, ':D')
+
+# 216 215에서 정의한 함수를 호출하라. 파라미터는 "안녕하세요"로 입력하라.
+print_with_smile('안녕하세요')
+
+# 217 현재 가격을 입력 받아 상한가 (30%)를 출력하는 print_upper_price 함수를 정의하라.
+def print_upper_price(price):
+    print(price*1.3)
+
+# 218 두 개의 숫자를 입력받아 두 수의 합을 출력하는 print_sum 함수를 정의하라.
+def print_sum(a,b):
+    print(a + b)
+
+# 219 두 개의 숫자를 입력받아 합/차/곱/나눗셈을 출력하는 print_arithmetic_operation 함수를 작성하라.
+#
+# print_arithmetic_operation(3, 4)
+# 3 + 4 = 7
+# 3 - 4 = -1
+# 3 * 4 = 12
+# 3 / 4 = 0.75
+def print_arithmetic_opearation(a,b):
+    print(a,'+',b,'=',a+b)
+    print(a,'-',b,'=',a-b)
+    print(a,'*',b,'=',a*b)
+    print(a,'/',b,'=',a/b)
+
+# 220 세 개의 숫자를 입력받아 가장 큰수를 출력하는 print_max 함수를 정의하라. 단 if 문을 사용해서 수를 비교하라.
+def print_max(a,b,c):
+    print(max(a,b,c))
